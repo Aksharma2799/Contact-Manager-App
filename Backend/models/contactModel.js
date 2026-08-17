5d762cc3af25b9ca7085323dee6bdbd1d9bd855d
aksharma2799 <rl.ashish01@gmail.com>
2026-08-17 23:28:39 +0530

    DB is fixed

--- Backend/models/contactModel.js
+++ Backend/models/contactModel.js
@@ -4,15 +4,15 @@ const contactSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: true,
+      required: [true, "Please add the contact name"],
     },
     email: {
       type: String,
-      required: true,
+      required: [true, "Please add the contact name"],
     },
     phone: {
       type: String,
-      required: true,
+      required: [true, "Please add the contact name"],
     },
   },
   {
